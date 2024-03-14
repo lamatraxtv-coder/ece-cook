05dbd168b7aec214b6a3c5f7f2b22704acd14e45
Louis Caubere-Nassah <ludwigcn2005@yahoo.com>
2024-03-14 12:13:35 +0100

    ajout des sous-programmes et du header pour les librairies

---
+++ cmake-build-debug/CMakeFiles/allegro.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/allegro.dir/main.c.obj"
+  "CMakeFiles/allegro.dir/main.c.obj.d"
+  "CMakeFiles/allegro.dir/sous-programme.c.obj"
+  "CMakeFiles/allegro.dir/sous-programme.c.obj.d"
+  "allegro.exe"
+  "allegro.exe.manifest"
+  "allegro.pdb"
+  "liballegro.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/allegro.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
