d07c3bcbaaa302db5897cd358a7e9da63224268d
Votre nom <arthur.portalier@edu.ece.fr>
2024-03-27 14:24:03 +0100

    fin du tuto (essayon de faire un tuto pour le temps et le score)

--- sous-programme.c
+++ sous-programme.c
@@ -282,23 +282,37 @@ void tuto(){
         if(key[KEY_LEFT]){
             compteur_page_tuto-=1;
         }
+        if(key[KEY_SPACE]){
+            destroy_bitmap(tutoP1);
+            destroy_bitmap(tutoP2);
+            destroy_bitmap(tutoP3);
+            destroy_bitmap(tutoP4);
+            return;
+        }
+        if(compteur_page_tuto>4){
+            compteur_page_tuto=4;
+        }
+        if(compteur_page_tuto<0){
+            compteur_page_tuto=0;
+        }
 
         switch(compteur_page_tuto){
             case 1:
                 blit(tutoP1, screen, 0, 0, (SCREEN_W - tutoP1->w) / 2, (SCREEN_H - tutoP1->h) / 2, tutoP1->w,tutoP1->h);
+                textprintf_ex(screen, font, 60, 100, makecol(0, 0, 0), -1, "espace pour revenir au menu");
                 break;
             case 2:
                 blit(tutoP2, screen, 0, 0, (SCREEN_W - tutoP2->w) / 2, (SCREEN_H - tutoP2->h) / 2, tutoP2->w, tutoP2->h);
+                textprintf_ex(screen, font, 60, 100, makecol(0, 0, 0), -1, "espace pour revenir au menu");
                 break;
             case 3:
                 blit(tutoP3, screen, 0, 0, (SCREEN_W - tutoP3->w) / 2, (SCREEN_H - tutoP3->h) / 2, tutoP3->w, tutoP3->h);
+                textprintf_ex(screen, font, 60, 100, makecol(0, 0, 0), -1, "espace pour revenir au menu");
                 break;
             case 4:
                 blit(tutoP4, screen, 0, 0, (SCREEN_W - tutoP4->w) / 2, (SCREEN_H - tutoP4->h) / 2, tutoP4->w, tutoP4->h);
+                textprintf_ex(screen, font, 60, 100, makecol(0, 0, 0), -1, "espace pour revenir au menu");
                 break;
-            default:
-                allegro_message("pas assez de page");
-                allegro_exit();
         }
 
     }
